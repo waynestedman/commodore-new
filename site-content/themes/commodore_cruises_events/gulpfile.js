12a666e2811cf9d32afb4224802a929e6d38ca41
Wayne Stedman <info@pacificwebfoundry.com>
2016-01-27 14:18:25 -0800

    Began Gulp switch
    Updated project and began switch over to Gulp.

---
+++ site-content/themes/commodore_cruises_events/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat');
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js',
+];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+		.pipe(coffee({ bare: true })
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/js'))
+});
