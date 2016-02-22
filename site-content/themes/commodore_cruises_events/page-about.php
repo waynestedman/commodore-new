05d88328664e75019140caf93e797fc86c5646c9
Wayne Stedman <info@pacificwebfoundry.com>
2016-02-22 14:19:55 -0800

    staff cards
    Staff cards create correctly.

--- site-content/themes/commodore_cruises_events/page-about.php
+++ site-content/themes/commodore_cruises_events/page-about.php
@@ -17,12 +17,8 @@ get_header(); ?>
 					<?php endwhile; // end of the loop. ?>
 				</article> 
 				<div class="cards">
-					<div class="card">
 					<?php 
 						$posts = get_posts(array(
-							//'posts_per_page' => 5,
-							//'offset' => 0,
-							//'numberposts' => -1,
 							'category_name' => 'executive',
 							'orderby' => 'ID',		
 							'post_type' => 'post',
@@ -32,29 +28,51 @@ get_header(); ?>
 					if($posts)
 					{ 
 						foreach ($posts as $post) {
+							echo '<div class="card">';
 			    			echo '<div class="card-image">';
 		      			echo '<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png" alt="">';
 			    			echo '</div>';
 							echo	'<div class="card-header">';
 							echo	'<h3>' .  the_field('name') . '</h3>';
-							echo	'<h3>' .  the_field('title') . '</h3>';
-							// echo '<p>' . the_content($post) . '</p>';
+							echo	'<p>' .  the_field('title') . '</p>';
+							echo '</div>';
+							echo '<div class="card-copy">';
+							echo '<p>' . the_field('description') . '</p>';
+							echo '</div>';
 							echo '</div>';
 						}
 					}
 					?>
-					
-	 				<div class="card">
-		    			<div class="card-image">
-		      			<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png" alt="">
-		    			</div> <!-- card-image -->
-		    			<div class="card-header">
-		      			First Card
-		    			</div> <!-- card-header -->
-		    			<div class="card-copy">
-		      			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident.</p>
-		    			</div> <!-- card-copy -->
-		  			</div> <!-- card -->
+				</div> <!-- Executive cards -->
+				<h3>Commodore "A" Team</h3>
+				<div class="cards">
+					<?php 
+						$posts = get_posts(array(
+							'posts_per_page' => 6,
+							'category_name' => 'staff',
+							'orderby' => 'ID',		
+							'post_type' => 'post',
+							'post_status' => 'publish'
+					));
+
+					if($posts)
+					{ 
+						foreach ($posts as $post) {
+							echo '<div class="card">';
+			    			echo '<div class="card-image">';
+		      			echo '<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png" alt="">';
+			    			echo '</div>';
+							echo	'<div class="card-header">';
+							echo	'<h3>' .  the_field('name') . '</h3>';
+							echo	'<p>' .  the_field('title') . '</p>';
+							echo '</div>';
+							echo '<div class="card-copy">';
+							echo '<p>' . the_field('description') . '</p>';
+							echo '</div>';
+							echo '</div>';
+						}
+					}
+					?>
 				</div> <!-- cards -->
 			</main><!-- #main -->
 		</section> <!-- section -->	
